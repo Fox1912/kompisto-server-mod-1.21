342e706c466aa7b1a1435d716cb49442a88e7e4b
Fox1912 <virtualfox1912@gmail.com>
2024-10-24 02:23:15 +0100

    Initial commit

---
+++ src/main/java/net/fox/kompistoservermod/KompistoServerMod.java
@@ -0,0 +1,18 @@
+package net.fox.kompistoservermod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KompistoServerMod implements ModInitializer {
+	public static final String MOD_ID = "kompisto-server-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Fuck off, cunt");
+
+	}
+}
---
+++ src/main/java/net/fox/kompistoservermod/KompistoServerModClient.java
@@ -0,0 +1,10 @@
+package net.fox.kompistoservermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KompistoServerModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
